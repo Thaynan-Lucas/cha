e1c88abfa09eb3f019e58f21f43768ac453e4e50
Thaynan-Lucas <thay.cross3@gmail.com>
2020-11-30 20:32:33 -0300

    Passo 1 - Criação Projeto

---
+++ src/main/java/com/cha/ChaApplication.java
@@ -0,0 +1,13 @@
+package com.cha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaApplication.class, args);
+	}
+
+}
